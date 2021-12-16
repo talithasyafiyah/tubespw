f220a7390bf9f4f5e69017fc4a39700d3d602667
Your Name <you@example.com>
2021-12-16 11:26:58 +0700

    PERBAIKAN DONNY

--- operator/in/receiving-details.php
+++ operator/in/receiving-details.php
@@ -131,7 +131,6 @@ while($data = mysqli_fetch_array($sql)){
                                        <div class="dropdown-menu">
                                           <a class="dropdown-item" href="../profileop.php">My Profile</a>
                                           <a class="dropdown-item" href="../settingsop.php">Settings</a>
-                                          <a class="dropdown-item" href="../help.php">Help</a>
                                           <a class="dropdown-item" href="../../pages/logout.php"><span>Log Out</span> <i class="fa fa-sign-out"></i></a>
                                        </div>
                                  </li>
@@ -191,7 +190,7 @@ while($data = mysqli_fetch_array($sql)){
 
                <div class="row column1">
                   <div class="col-lg-12">
-                     <div class="white_shd full margin_bottom_30">
+                     <div class="white_shd full margin_bottom_30" style="background-image: url('../images/layout_img/pattern_h.png');">
                            <div class="full graph_head">
                               <div class="heading1 margin_0">
                                  <h2>
